af1ada268d6f1c02ae45a4903ea74331d372a7a1
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2019-01-05 17:16:29 -0800

    create initial test extending StartingCapabilities class and declaring the Capabilities method

---
+++ src/BasicsOfIdObjectsAndFirstTestWithAppium.java
@@ -0,0 +1,15 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class BasicsOfIdObjectsAndFirstTestWithAppium extends StartingCapabilities{
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		AndroidDriver<AndroidElement> driver = Capabilities();
+		
+	}
+
+}
